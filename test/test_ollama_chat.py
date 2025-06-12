375ceff13abcb0b3920644d193c174f96373a593
ChairC <974833488@qq.com>
2025-06-12 21:17:06 +0800

    feat: add chat.

---
+++ test/test_ollama_chat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+    @Date   : 2025/5/22 10:29
+    @Author : chairc
+    @Site   : https://github.com/chairc
+"""
+from unittest import TestCase
+
+
+class TestOllamaChat(TestCase):
+    def test_chat(self):
+        self.fail()
+
+    def test_chat_async(self):
+        self.fail()
