d2c56a715bae1fc6204d0aa92f2bfbc9bed991f4
ChairC <974833488@qq.com>
2025-06-12 21:19:17 +0800

    chore: modify the example statement and add dict example.

--- infinite/entities/r.py
+++ infinite/entities/r.py
@@ -12,15 +12,19 @@ class R:
     Response class
     Use this class to return a response in the web api
     """
+
     def __init__(self, code=0, msg="", data=None):
         """
         Response class
         :param code: Response code
         :param msg: Response message
         :param data: Response data
-        >> R(code=10000, msg="WOW", data={123, 234}).to_dict()
-        >> R().success(msg="111", data="111")
-        >> R().fail(code=400, msg="222")
+
+        **Example**::
+
+            >>> R(code=10000, msg="WOW", data={123, 234}).to_dict()
+            >>> R().success(msg="111", data="111")
+            >>> R().fail(code=400, msg="222")
         """
         self.code = code
         self.msg = msg
@@ -65,5 +69,6 @@ class R:
 
 if __name__ == "__main__":
     print(R(code=10000, msg="WOW", data={123, 234}).to_dict())
+    print(R(code=10001, msg="DICT", data={"key_0": "value_0", "key_1": "value_1"}).to_dict())
     print(R().success(msg="111", data="111"))
     print(R().fail(code=400, msg="222"))
